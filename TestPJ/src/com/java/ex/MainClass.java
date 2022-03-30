b3a5e6e773c877059fe71c99a6f3a111c1643258
Dansunmi <27815273+dansunmi@users.noreply.github.com>
2022-03-30 09:43:55 +0900

    ddd
    ddd

---
+++ TestPJ/src/com/java/ex/MainClass.java
@@ -0,0 +1,19 @@
+package com.java.ex;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 2;
+		System.out.println("합: "+ (a+b));
+		int c = 13;
+		int d = 14;
+		System.out.println("합합: "+ (c+d));
+		while(1){
+			System.out.println("밀려드는 수업");
+		}
+		
+
+	}
+
+}
